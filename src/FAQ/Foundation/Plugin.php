1cf8be98d35cccc753322cddba5542c67047cca0
Simon <simon@yard.nl>
2025-03-25 13:51:02 +0100

    (chore): v3.1.3

--- src/FAQ/Foundation/Plugin.php
+++ src/FAQ/Foundation/Plugin.php
@@ -27,7 +27,7 @@ class Plugin extends BasePlugin
      *
      * @const string VERSION
      */
-    const VERSION = '3.1.1';
+    const VERSION = '3.1.3';
 
     protected function checkForUpdate()
     {
